a0e9c240a162bd27d5ae4d9e4fa0d0f236b8b27d
Olwiba <olwiba@gmail.com>
2026-09-11 21:48:03 +0100

    chore(site): show the version pill beside the credit line

--- site/components/SiteFooter.tsx
+++ site/components/SiteFooter.tsx
@@ -8,6 +8,7 @@ export function SiteFooter() {
       versions={[
         { version, href: 'https://github.com/Olwiba/olwibaUI/blob/master/CHANGELOG.md' },
       ]}
+      changelogUrl="https://github.com/Olwiba/olwibaUI/blob/master/CHANGELOG.md"
       links={[
         {
           label: '🪲 Report a bug',
